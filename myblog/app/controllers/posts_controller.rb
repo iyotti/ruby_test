aeae9a9ef49593c131cd25b6a3af954fd6a10e1e
iyotti <neuroooo100@gmail.com>
2018-10-27 22:49:05 +0900

    update:ドットインストール　~#18

--- myblog/app/controllers/posts_controller.rb
+++ myblog/app/controllers/posts_controller.rb
@@ -8,9 +8,24 @@ class PostsController < ApplicationController
 	end
 
 	def new
-		
+		@post = Post.new
 	end
 
 	def create
+		#render plain: params[:post].inspect
+		#save
+		@post = Post.new(post_params)
+		if @post.save
+			#redirect
+			redirect_to posts_path
+		else
+			#render plain: @post.errors.inspect
+			render 'new'
+		end
 	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 end
