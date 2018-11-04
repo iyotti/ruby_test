0db60995247c8ae48c08bc26cc324a037bf9d133
iyotti <neuroooo100@gmail.com>
2018-11-04 20:43:40 +0900

    update:ドットインストール ~#28

--- myblog/app/controllers/posts_controller.rb
+++ myblog/app/controllers/posts_controller.rb
@@ -36,6 +36,14 @@ class PostsController < ApplicationController
 			render 'edit'
 		end
 	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end
+
+
 	private
 		def post_params
 			params.require(:post).permit(:title, :body)
